3c45593ca0dbdcdf56379387fa4b9834b85a3d5e
takorteam <kteam@tecace.com>
2023-06-26 15:18:30 +0900

    On Host 방식으론 Test connet 가 안되서 다시 container 방법으로 변경 후 TEST

--- Dockerfile
+++ Dockerfile
@@ -2,7 +2,11 @@ FROM node:latest
 
 WORKDIR /usr/src/app
 COPY package*.json ./
-RUN npm install 
+RUN npm install
+ENV NEW_RELIC_NO_CONFIG_FILE=true
+ENV NEW_RELIC_DISTRIBUTED_TRACING_ENABLED=true \
+NEW_RELIC_LOG=stdout
+# etc.
 
 COPY . .
 
--- start.sh
+++ start.sh
@@ -1,16 +1,7 @@
 #!/bin/sh
 
-node -r newrelic server.js
-node --experimental-loader=newrelic/esm-loader.mjs server.js
-docker run \
-  -d \
-  --name newrelic-infra \
-  --network=host \
-  --cap-add=SYS_PTRACE \
-  --privileged \
-  --pid=host \
-  -v "/:/host:ro" \
-  -v "/var/run/docker.sock:/var/run/docker.sock" \
-  -e NRIA_LICENSE_KEY=2cf598354e84137ebf051624638e296cFFFFNRAL \
-  newrelic/infrastructure:latest
+docker run -e NEW_RELIC_LICENSE_KEY=2cf598354e84137ebf051624638e296cFFFFNRAL \
+  -e NEW_RELIC_APP_NAME="sdp-apm" \
+  sdptestregistry/myapp:3bc358707ed85c0f195f6f55fccbdbde9d92492e
+
 npm start
