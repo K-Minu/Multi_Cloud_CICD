86856cad200f136365325bd0415538ddf44cd979
takorteam <kteam@tecace.com>
2023-07-21 08:41:14 +0900

    Client API TEST Code

--- server.js
+++ server.js
@@ -1,15 +1,44 @@
 "use strict";
 
-const express = require("express");
-// Constants
-const PORT = 80;
+// const express = require("express");
+// // Constants
+// const PORT = 80;
 
-// App
-const app = express();
-app.get("/", (req, res) => {
-  res.sendFile(__dirname + "/index.html");
+// // App
+// const app = express();
+// app.get("/", (req, res) => {
+//   res.sendFile(__dirname + "/index.html");
+// });
+
+// app.listen(PORT, () => {
+//   console.log(`Server1 is running on >> http://localhost:${PORT}`);
+// });
+
+const Koa = require("koa");
+const Router = require("koa-router");
+const app = new Koa();
+const router = new Router();
+const port = 80;
+const { Prometheus } = require("./metrics");
+
+router.get("/", async (ctx) => {
+  ctx.body = "Home";
 });
 
-app.listen(PORT, () => {
-  console.log(`Server1 is running on >> http://localhost:${PORT}`);
+router.get("/metrics", async (ctx) => {
+  const { metrics, contentType } = await Prometheus.get();
+  ctx.set("Content-Type", contentType);
+  ctx.body = metrics;
+});
+router.get("/metrics/:name", (ctx) => {
+  const { name } = ctx.params;
+  const randomNumber = Math.round(Math.random() * 10);
+  console.info(name, randomNumber);
+  Prometheus.add({ name, data: randomNumber });
+  ctx.body = "done";
+});
+app.use(router.routes());
+app.use(router.allowedMethods());
+app.listen(port, () => {
+  console.info(`Server is running on >> http://localhost:${port}`);
 });
