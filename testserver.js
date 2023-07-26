522693225fa8879ac083198b30382c9caf040348
takorteam <kteam@tecace.com>
2023-07-26 10:04:57 +0900

    Update testserver

--- testserver.js
+++ testserver.js
@@ -5,10 +5,17 @@ const Prometheus = require("prom-client");
 
 const app = express();
 const port = 4000;
+const metricsInterval = Prometheus.collectDefaultMetrics();
 const checkoutsTotal = new Prometheus.Counter({
   name: "checkouts_total",
   help: "Total number of checkouts",
   labelNames: ["payment_method"],
+});
+const httpRequestDurationMicroseconds = new Prometheus.Histogram({
+  name: "http_request_duration_ms",
+  help: "Duration of HTTP requests in ms",
+  labelNames: ["method", "route", "code"],
+  buckets: [0.1, 5, 15, 50, 100, 200, 300, 400, 500], // buckets for response time from 0.1ms to 500ms
 });
 
 // Runs before each requests
@@ -56,6 +63,31 @@ app.use((err, req, res, next) => {
   next();
 });
 
+// Runs after each requests
+app.use((req, res, next) => {
+  const responseTimeInMs = Date.now() - res.locals.startEpoch;
+
+  httpRequestDurationMicroseconds
+    .labels(req.method, req.route, res.statusCode)
+    .observe(responseTimeInMs);
+
+  next();
+});
+
 const server = app.listen(port, () => {
   console.log(`Example app listening on port ${port}!`);
+});
+
+// Graceful shutdown
+process.on("SIGTERM", () => {
+  clearInterval(metricsInterval);
+
+  server.close((err) => {
+    if (err) {
+      console.error(err);
+      process.exit(1);
+    }
+
+    process.exit(0);
+  });
 });
